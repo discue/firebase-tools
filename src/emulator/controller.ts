89a007f9f966f40a16805957ce5e170ba765dd7d
Chris Thompson <christhompson@google.com>
2022-08-16 16:44:40 -0700

    Cleanup

--- src/emulator/controller.ts
+++ src/emulator/controller.ts
@@ -366,9 +366,7 @@ function findExportMetadata(importPath: string): ExportMetadata | undefined {
 }
 
 interface EmulatorOptions extends Options {
-  // FIXME who converts this from the config then?
   extDevEnv?: Record<string, string>;
-  singleProjectMode?: boolean;
 }
 
 /**
