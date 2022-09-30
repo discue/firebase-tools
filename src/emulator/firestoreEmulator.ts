a355224d2b9cdff989119af6c6a20e09888abc0a
Chris Thompson <christhompson@google.com>
2022-09-30 10:04:10 -0700

    Removed flag and added config value singleProjectMode.

--- src/emulator/firestoreEmulator.ts
+++ src/emulator/firestoreEmulator.ts
@@ -21,7 +21,7 @@ export interface FirestoreEmulatorArgs {
   auto_download?: boolean;
   seed_from_export?: string;
   single_project_mode?: boolean;
-  single_project_mode_error?: boolean;
+  single_project_mode_error?: boolean; // FIXME debug report missing project ID
 }
 
 export class FirestoreEmulator implements EmulatorInstance {
